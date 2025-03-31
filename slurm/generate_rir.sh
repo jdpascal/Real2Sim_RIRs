7f237c768e963c866ea5ddaf41c95fb3be8c79c9
JD <jeandaniel.pascal@cerema.fr>
2025-03-31 14:26:54 +0200

    slurm generate rir

---
+++ slurm/generate_rir.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+#SBATCH -p publicgpu
+#SBATCH --nodes=2               # This needs to match --ddp-nodes
+#SBATCH --ntasks-per-node=4     # This needs to match --ddp-devices-per-node
+#SBATCH --gres=gpu:4            # Request N GPUs per machine
+#SBATCH --constraint=gputc
+#SBATCH --mem=0
+#SBATCH --time=0-02:00:00
+
+# Load correct python and cuda modules
+module load python/3.12.8
+module load cuda/cuda-12.1
+
+# Activate python environment
+source venv/bin/activate
+
+python RIR_generation/generate_rirs.py
